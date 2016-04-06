7369b7836ec73e76367eca68c716f5e907dba302
Nathan Potter <ndp@opendap.org>
2016-04-06 12:19:55 -0700

    Fixed problems in linear_scale by adding code to look at both DAP4 and DAP2 attribtes.

--- functions/LinearScaleFunction.cc
+++ functions/LinearScaleFunction.cc
@@ -28,6 +28,8 @@
 #include <sstream>
 
 #include <BaseType.h>
+#include <DMR.h>
+#include <D4Group.h>
 #include <Float64.h>
 #include <Str.h>
 #include <Array.h>
@@ -36,6 +38,7 @@
 
 #include <Error.h>
 #include <DDS.h>
+#include <D4Attributes.h>
 
 #include <debug.h>
 #include <util.h>
@@ -44,7 +47,7 @@
 
 #include "LinearScaleFunction.h"
 
-#define DEBUG_KEY "function"
+#define DEBUG_KEY "functions"
 
 using namespace libdap;
 
@@ -81,29 +84,66 @@ static double string_to_double(const char *val)
  @param var Look for attributes in this BaseType variable.
  @param attributes A vector of attributes; the first one found will be returned.
  @return The attribute value in a double. */
-static double get_attribute_double_value(BaseType *var, vector<string> &attributes)
-{
+static double get_attribute_double_value(BaseType *var,
+        vector<string> &attributes) {
     // This code also builds a list of the attribute values that have been
     // passed in but not found so that an informative message can be returned.
     AttrTable &attr = var->get_attr_table();
-    string attribute_value = "";
-    string values = "";
-    vector<string>::iterator i = attributes.begin();
-    while (attribute_value == "" && i != attributes.end()) {
-        values += *i;
-        if (!values.empty()) values += ", ";
-        attribute_value = attr.get_attr(*i++);
+    string attribute_value = "", attribute_name="";
+    string checkedAttrNames = "";
+
+    D4Attributes *d4Attr = var->attributes();
+    if(!d4Attr->empty()){
+        BESDEBUG(DEBUG_KEY, "get_attribute_double_value() - Checking DAP4 Attributes (DAP4 attribute table is not empty.)" << endl);
+        vector<string>::iterator i = attributes.begin();
+        while (attribute_value == "" && i != attributes.end()) {
+            /// This is building an error message that may not be used.
+            string aName = *i;
+            checkedAttrNames += aName;
+            if (!checkedAttrNames.empty())
+                checkedAttrNames += ", ";
+
+           BESDEBUG(DEBUG_KEY, "get_attribute_double_value() - Looking for DAP4 attribute: '" << aName << "'"<< endl);
+           D4Attribute *requestedAttr = d4Attr->find(aName);
+           if(requestedAttr!=0){
+               BESDEBUG(DEBUG_KEY, "get_attribute_double_value() - Found for DAP4 attribute: '" << aName << "'"<< endl);
+               attribute_value = requestedAttr->value(0);
+               attribute_name = aName;
+           }
+           else {
+               BESDEBUG(DEBUG_KEY, "get_attribute_double_value() - MISSING DAP4 attribute: '" << aName << "'"<< endl);
+           }
+           ++i;
+        }
+
+        BESDEBUG(DEBUG_KEY, "get_attribute_double_value() - Using DAP4 attribute: '"<< attribute_name << "' value: " << attribute_value << endl);
     }
+    else {
+        BESDEBUG(DEBUG_KEY, "get_attribute_double_value() - Checking DAP2 attributes." << endl);
+       vector<string>::iterator i = attributes.begin();
+        while (attribute_value == "" && i != attributes.end()) {
+            /// This is building an error message that may not be used.
+            checkedAttrNames += *i;
+            if (!checkedAttrNames.empty())
+                checkedAttrNames += ", ";
+
+            attribute_value = attr.get_attr(*i++);
+        }
+        BESDEBUG(DEBUG_KEY, "get_attribute_double_value() - Using DAP2 attribute: '"<< *i << "' value: " << attribute_value << endl);
+    }
+
 
     // If the value string is empty, then look at the grid's array (if it's a
     // grid) or throw an Error.
     if (attribute_value.empty()) {
         if (var->type() == dods_grid_c)
-            return get_attribute_double_value(dynamic_cast<Grid&>(*var).get_array(), attributes);
+            return get_attribute_double_value( dynamic_cast<Grid&>(*var).get_array(), attributes);
         else
             throw Error(malformed_expr,
-                    string("No COARDS/CF '") + values.substr(0, values.length() - 2)
-                            + "' attribute was found for the variable '" + var->name() + "'.");
+                    string("No COARDS/CF '")
+                            + checkedAttrNames.substr(0, checkedAttrNames.length() - 2)
+                            + "' attribute was found for the variable '"
+                            + var->name() + "'.");
     }
 
     return string_to_double(remove_quotes(attribute_value).c_str());
@@ -111,8 +151,21 @@ static double get_attribute_double_value(BaseType *var, vector<string> &attribut
 
 static double get_attribute_double_value(BaseType *var, const string &attribute)
 {
-    AttrTable &attr = var->get_attr_table();
-    string attribute_value = attr.get_attr(attribute);
+    string attribute_value;
+
+    D4Attributes *d4Attr = var->attributes();
+    if(!d4Attr->empty()){
+        // BESDEBUG(DEBUG_KEY, "get_attribute_double_value() - D4Attributes: "<< endl << *d4Attr << endl);
+        D4Attribute *requestedAttr = d4Attr->find(attribute);
+        if(requestedAttr!=0)
+            attribute_value = requestedAttr->value(0);
+        BESDEBUG(DEBUG_KEY, "get_attribute_double_value() - Using DAP4 attribute: '"<< attribute << "' value: " << attribute_value << endl);
+    }
+    else {
+        AttrTable &attr = var->get_attr_table();
+        string attribute_value = attr.get_attr(attribute);
+        BESDEBUG(DEBUG_KEY, "get_attribute_double_value() - Using DAP2 attribute: '"<< attribute << "' value: " << attribute_value << endl);
+    }
 
     // If the value string is empty, then look at the grid's array (if it's a
     // grid or throw an Error.
@@ -133,17 +186,23 @@ static double get_y_intercept(BaseType *var)
     vector<string> attributes;
     attributes.push_back("add_offset");
     attributes.push_back("add_off");
-    return get_attribute_double_value(var, attributes);
+    double b =  get_attribute_double_value(var, attributes);
+    BESDEBUG(DEBUG_KEY, "get_y_intercept() - returning: " << b << endl);
+    return b;
 }
 
 static double get_slope(BaseType *var)
 {
-    return get_attribute_double_value(var, "scale_factor");
+    double d =  get_attribute_double_value(var, "scale_factor");
+    BESDEBUG(DEBUG_KEY, "get_slope() - returning: " << d << endl);
+    return d;
 }
 
 static double get_missing_value(BaseType *var)
 {
-    return get_attribute_double_value(var, "missing_value");
+    double mv = get_attribute_double_value(var, "missing_value");
+    BESDEBUG(DEBUG_KEY, "get_missing_value() - returning: " << mv << endl);
+    return mv;
 }
 
 BaseType *compute_linear_scale_result(BaseType *bt, double m, double b, double missing, bool use_missing)
@@ -156,9 +215,9 @@ BaseType *compute_linear_scale_result(BaseType *bt, double m, double b, double m
         // Grab the whole Grid; note that the scaling is done only on the array part
         Grid &source = dynamic_cast<Grid&>(*bt);
 
-        BESDEBUG(DEBUG_KEY, "function_linear_scale_worker() - Grid send_p: " << source.send_p() << endl);
+        BESDEBUG(DEBUG_KEY, "compute_linear_scale_result() - Grid send_p: " << source.send_p() << endl);
         BESDEBUG(DEBUG_KEY,
-                "function_linear_scale_worker() - Grid Array send_p: " << source.get_array()->send_p() << endl);
+                "compute_linear_scale_result() - Grid Array send_p: " << source.get_array()->send_p() << endl);
 
         // Read the grid; set send_p since Grid is a kind of constructor and
         // read will only be called on it's fields if their send_p flag is set
@@ -187,9 +246,9 @@ BaseType *compute_linear_scale_result(BaseType *bt, double m, double b, double m
         delete[] data;
 
         // FIXME result->set_send_p(true);
-        BESDEBUG(DEBUG_KEY, "function_linear_scale_worker() - Grid send_p: " << source.send_p() << endl);
+        BESDEBUG(DEBUG_KEY, "compute_linear_scale_result() - Grid send_p: " << source.send_p() << endl);
         BESDEBUG(DEBUG_KEY,
-                "function_linear_scale_worker() - Grid Array send_p: " << source.get_array()->send_p() << endl);
+                "compute_linear_scale_result() - Grid Array send_p: " << source.get_array()->send_p() << endl);
 
         dest = result;
     }
@@ -265,6 +324,8 @@ BaseType *linear_scale_worker(vector<BaseType*> argv){
         use_missing = false;
     }
     else {
+        //BESDEBUG(DEBUG_KEY, "linear_scale_worker() - argv[0]: "<< *argv[0] << endl);
+
         m = get_slope(argv[0]);
 
         // This is really a hack; on a fair number of datasets, the y intercept
@@ -314,11 +375,11 @@ void function_dap2_linear_scale(int argc, BaseType * argv[], DDS &, BaseType **b
 {
     BESDEBUG(DEBUG_KEY, "function_dap2_linear_scale() - BEGIN" << endl);
 
-    BESDEBUG(DEBUG_KEY, "function_dap2_linear_scale() - Building argument vector for grid_worker()" << endl);
+    BESDEBUG(DEBUG_KEY, "function_dap2_linear_scale() - Building argument vector for linear_scale_worker()" << endl);
     vector<BaseType*> args;
     for(int i=0; i< argc; i++){
         BaseType * bt = argv[i];
-        BESDEBUG(DEBUG_KEY, "function_dap2_linear_scale() - Adding argument: "<< bt->name() << endl);
+        BESDEBUG(DEBUG_KEY, "function_dap2_linear_scale() - BaseType Retrieved from DDS: "<< *bt << endl);
         args.push_back(bt);
     }
 
@@ -339,17 +400,22 @@ void function_dap2_linear_scale(int argc, BaseType * argv[], DDS &, BaseType **b
  numeric scalar, Array or Grid. */
 BaseType *function_dap4_linear_scale(D4RValueList *dvl_args, DMR &dmr)
 {
-    BESDEBUG(DEBUG_KEY, "function_dap4_linear_scale() - Building argument vector for grid_worker()" << endl);
+    BESDEBUG(DEBUG_KEY, "function_dap4_linear_scale() - Building argument vector for linear_scale_worker()" << endl);
+    //BESDEBUG(DEBUG_KEY, "function_dap4_linear_scale() - DMR.root(): "<< endl << *dmr.root() << endl);
+
+
     vector<BaseType*> args;
     for(unsigned int i=0; i< dvl_args->size(); i++){
         BaseType * bt = dvl_args->get_rvalue(i)->value(dmr);
-        BESDEBUG(DEBUG_KEY, "function_dap4_grid() - Adding argument: "<< bt->name() << endl);
+        BESDEBUG(DEBUG_KEY, "function_dap4_linear_scale() - Adding argument: "<< bt->name() << endl);
+        BESDEBUG(DEBUG_KEY, "function_dap4_linear_scale() - BaseType Retrieved from DDS: "<< *bt << endl);
         args.push_back(bt);
+        bt = dmr.root()->var(bt->name());
     }
 
     BaseType *result = linear_scale_worker(args);
 
-    BESDEBUG(DEBUG_KEY, "function_dap4_grid() - END (result: "<< result->name() << ")" << endl);
+    BESDEBUG(DEBUG_KEY, "function_dap4_linear_scale() - END (result: "<< result->name() << ")" << endl);
     return result;
 
 
